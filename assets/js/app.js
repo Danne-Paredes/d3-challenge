845f5733b7bd5332859fc5fba911fb831c2287e2
Danne <danneparedes@gmail.com>
2020-08-04 17:23:30 -0700

    finished core assignment

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,11 +36,11 @@ d3.csv("data.csv").then(function(newData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(newData, d => d.poverty)])
+      .domain([8, d3.max(newData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(newData, d => d.obesity)])
+      .domain([20, d3.max(newData, d => d.obesity)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -59,23 +59,36 @@ d3.csv("data.csv").then(function(newData) {
 
     // Step 5: Create Circles
     // ==============================
-    var circlesGroup = chartGroup.selectAll("circle")
-    .data(newData)
-    .enter()
-    .append("circle")
+
+    var node = chartGroup.selectAll("g")
+                .data(newData)
+                .enter()
+                .append("g");
+
+    var circlesGroup = node.append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.obesity))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
+    .attr("r", "20")
+    .attr("fill", "blue")
+    .attr("opacity", ".9")
+    .attr('class','stateCircle');
+
+// used answer from here to add labels: https://stackoverflow.com/questions/12266967/d3-js-how-to-add-labels-to-scatter-points-on-graph
+    node.append("text")
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.obesity))
+    .attr('class','stateText')
+    .text(function(d) { return d.abbr; });
+
 
     // Step 6: Initialize tool tip
     // ==============================
     var toolTip = d3.tip()
       .attr("class", "tooltip")
+      .attr('class','d3-tip')
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.rockband}<br>Hair length: ${d.poverty}<br>Hits: ${d.obesity}`);
+        return (`${d.state}<br>Poverty: ${d.poverty}%<br>Obesity: ${d.obesity}%`);
       });
 
     // Step 7: Create tooltip in the chart
@@ -84,7 +97,7 @@ d3.csv("data.csv").then(function(newData) {
 
     // Step 8: Create event listeners to display and hide the tooltip
     // ==============================
-    circlesGroup.on("click", function(data) {
+    node.on("click", function(data) {
       toolTip.show(data, this);
     })
       // onmouseout event
@@ -99,12 +112,14 @@ d3.csv("data.csv").then(function(newData) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+      .attr("class", "active")
+      .text("In Poverty(%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
+      .attr("class", "active")
+      .text('Is Obese(%)');
   }).catch(function(error) {
     console.log(error);
   });
